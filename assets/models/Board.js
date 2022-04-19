d1aef6f820387da7d72f3641f1bf9f470b5c81f5
Hugo Ernesto <mail.hbler@gmail.com>
2022-04-19 12:26:39 -0300

    update: board model updated

--- assets/models/Board.js
+++ assets/models/Board.js
@@ -1,12 +1,63 @@
 class Board {
-  cols;
-  rows;
-  constructor(cols, rows) {
-    this.cols = cols;
-    this.rows = rows;
-    this.buildBoard();
+  _cols;
+  _rows;
+  _players;
+  constructor(cols, rows, players) {
+    this._cols = cols;
+    this._rows = rows;
+    this._players = players;
+    this._currentPlayer = this._players[0];
+    this._map = createEmptyMap();
+    this._buildBoard();
   }
 
+  // getters
+  get columns() {
+    return this._cols;
+  }
+  get rows() {
+    return this._rows;
+  }
+  get players() {
+    return this._players;
+  }
+
+  get map() {
+    return this._map;
+  }
+
+  get currentPlayer() {
+    return this._currentPlayer;
+  }
+
+  // setters
+  set columns(_) {
+    throw "Can't change this value";
+  }
+
+  set rows(_) {
+    throw "Can't change this value";
+  }
+
+  set players(_) {
+    throw "Can't change this value";
+  }
+
+  set map(_) {
+    throw "Can't change this value";
+  }
+
+  set currentPlayer(player) {
+    if (player) this._currentPlayer = player;
+  }
   //methods
+  createEmptyMap() {
+    const map = [];
+    for (let i = 0; i < this.rows; i++) {
+      map.push(new Array(this.columns));
+    }
+    return map;
+  }
+
   buildBoard() {}
 }
